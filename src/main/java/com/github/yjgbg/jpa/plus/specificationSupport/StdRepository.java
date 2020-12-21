d1bf26e5a23f0899729fe9b49adbc4354047ba11
yjgbg <1239461052@qq.com>
2020-12-21 17:13:07 +0800

    解耦ActiveEntity，适配springboot2.1.3

--- src/main/java/com/github/yjgbg/jpa/plus/specificationSupport/StdRepository.java
+++ src/main/java/com/github/yjgbg/jpa/plus/specificationSupport/StdRepository.java
@@ -1,12 +1,12 @@
 package com.github.yjgbg.jpa.plus.specificationSupport;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.Repository;
 
 @NoRepositoryBean
 public interface StdRepository<T>
-    extends JpaSpecificationExecutor<T>, Repository<T, Long> {
+    extends JpaSpecificationExecutor<T>, JpaRepository<T, Long> {
   /**
    * 获取到一个可执行(find，count等函数)的Specification
    * @return

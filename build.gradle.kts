4a16f1d06765dd7e6c705369826903a1620c2004
weicl <1239461052@qq.com>
2021-10-11 18:32:27 +0800

    2.5.5 spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.yjgbg"
-version = "hypers-2.5.4-SNAPSHOT"
+version = "hypers-2.5.5-SNAPSHOT"
 description = "jpa-plus-spring-boot-starter"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
@@ -16,7 +16,7 @@ repositories {
 dependencies {
     api("com.github.yjgbg:active-entity:1.3")
     api("com.google.guava:guava:30.1.1-jre")
-    api("org.springframework.boot:spring-boot-starter-data-jpa:2.5.4")
+    api("org.springframework.boot:spring-boot-starter-data-jpa:2.5.5")
     compileOnly("org.jetbrains:annotations:+")
     compileOnly("org.projectlombok:lombok:+")
     annotationProcessor("org.projectlombok:lombok:+")

9ed8de4fc4f25f8316c051c7a50993345c884b80
weicl <chenglong.wei@mail.hypers.com>
2022-04-13 12:23:21 +0800

    添加了发布到hypers maven仓库的gradle配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.github.yjgbg"
-version = "2.1.3.008-SNAPSHOT"
+version = "2.1.3.007-SNAPSHOT"
 description = "jpa-plus-spring-boot-starter"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
@@ -25,13 +25,31 @@ java {
 }
 
 publishing {
-    publications.create<MavenPublication>("this") {
+    publications.create<MavenPublication>("snapshot") {
         from(components["java"])
+        pom {
+            version = "${project.ext["publicationVersion"].toString()}-SNAPSHOT"
+        }
+    }
+    publications.create<MavenPublication>("hypers") {
+        from(components["java"])
+        pom {
+            groupId = "com.hypers.weicl"
+            version = project.ext["publicationVersion"].toString()
+        }
     }
     repositories.maven("https://oss.sonatype.org/content/repositories/snapshots") {
+        name = "snapshot"
         credentials {
             username = project.ext["mavenUsername"].toString()
             password = project.ext["mavenPassword"].toString()
+        }
+    }
+    repositories.maven("https://nexus3.hypers.cc/repository/maven-releases/") {
+        name = "hypers"
+        credentials {
+            username = project.ext["hypersMavenUsername"].toString()
+            password = project.ext["hypersMavenPassword"].toString()
         }
     }
 }
